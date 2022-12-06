cfefa06d71b94bf4c6f25483e5f89ecddcd4b6a6
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2022-12-06 16:38:45 +0900

    workflow update

--- .github/extract_hash.sh
+++ .github/extract_hash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-grep "branch=master\?#[a-f0-9]*" Cargo.lock > iced_hash.txt
+grep -o "branch=master\?#[a-f0-9]*" Cargo.lock > iced_hash.txt
 cat iced_hash.txt
 sed '1!d' iced_hash.txt > tmp0.txt
 sed 's/branch=master?#//g' > tmp1.txt
