fbc85bff9d67b69c0fda98ab92ae7dfdd8e10dbc
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2022-12-06 14:57:09 +0900

    extract hash by discrete script

---
+++ .github/extract_hash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+grep "branch=master\?#[a-f0-9]*" Cargo.lock > iced_hash.txt
+cat iced_hash.txt
+sed '1!d' iced_hash.txt > tmp0.txt
+sed 's/branch=master?#//g' > tmp1.txt
+cat tmp1.txt
