2ff2a419612a4bc9e90b5747bb1eccff823aab97
triangle drawer <48007646+t18b219k@users.noreply.github.com>
2022-12-06 17:20:18 +0900

    fixed to upload linux binary and include iced hash

--- .github/extract_hash.sh
+++ .github/extract_hash.sh
@@ -3,5 +3,5 @@
 grep -o "branch=master\?#[a-f0-9]*" Cargo.lock > iced_hash.txt
 cat iced_hash.txt
 sed '1!d' iced_hash.txt > tmp0.txt
-sed 's/branch=master?#//g' > tmp1.txt
+sed 's/branch=master#//g' tmp0.txt> tmp1.txt
 cat tmp1.txt
